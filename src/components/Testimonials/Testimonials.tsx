0a9c781acee447f36f05c04215d7a48cc7a49e28
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2022-05-04 22:53:32 +0000

    [Fix] Remove image-margin-auto when in desktop

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -15,14 +15,14 @@ const Testimonials = () => {
                 File storage made easy - including powerful features you won't anywhere else you are
             </p>
             </div>
-            <div className='mt-[100px] lg:flex lg:gap-5 items-start '>
-                <div className='w-72 h-96 rounded-lg overflow-hidden mx-auto flex-shrink-0'>
+            <div className='mt-[100px] lg:flex lg:gap-10 lg:items-start lg:justify-center '>
+                <div className='w-72 h-96 rounded-lg overflow-hidden mx-auto  lg:mx-0'>
 
                 <img src={Client}  className="object-fill w-full " />
                 </div>
-                <div className='flex-shrink-0 lg:max-w-md'>
+                <div className=' lg:max-w-md '>
                     <p className='text-[18px] '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum suscipit in debitis earum officiis dolorem reiciendis impedit, sed eveniet dolor ipsum ipsa molestias dolorum labore error non animi reprehenderit? Et.</p>
-                    <p className='text-[22px] font-bold mt-4'> Larry Page</p>
+                    <p className='text-[22px] font-bold mt-2'> Larry Page</p>
                     <p className='text-[14px]'> Chief Executive Officer</p>
                     <div className='flex gap-2 items-center mt-5'>
                         <button className='bg-white outline-none rounded-full p-3'>
